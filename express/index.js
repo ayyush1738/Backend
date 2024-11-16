dd11fd860102192903613e591f8c704088ca7d01
ayyush1738 <singhrathoreayush824@gmail.com>
2024-11-16 15:38:50 +0530

    Ayush

--- express/index.js
+++ express/index.js
@@ -2,15 +2,53 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send("Hello world!");
+let coffeeData = [];
+let nextId = 1;
+
+app.post('/coffee', (req, res) => {
+    const { name, price } = req.body;
+    const newCoffee = { id: nextId++, name, price };
+    coffeeData.push(newCoffee);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).json(newCoffee); // This sets the response status and sends the data in JSON format
+});
+
+app.get('/coffee', (req, res) => {
+    res.status(200).json(coffeeData);
+});
+
+app.get('/coffee/:id', (req, res) => {
+    const coffee = coffeeData.find(t => t.id === parseInt(req.params.id));
+    if (!coffee) {
+        return res.status(404).send("Coffee not found!");
+    }
+    res.status(200).json(coffee);
+});
+
+app.put('/coffee/:id', (req, res)=>{
+    const coffee = coffeeData.find(t => t.id === parseInt(req.params.id));
+    if(!coffee)
+    {
+        return res.status(404).send("coffee not there");
+    }
+    const {name, price} = req.body;
+    coffee.name = name;
+    coffee.price = price;
+    res.status(200).json(coffee);
 })
 
-app.get('/buy-coffee', (req, res)=>{
-    res.send("Thanks for buying the coffee!");
+app.delete('/coffee/:id', (req, res)=>{
+    const index = coffeeData.findIndex(t=> t.id === parseInt(req.params.id));
+    if(index==-1)
+    {
+        return res.status(404).send('Coffee not there in the list!');
+    }
+    coffeeData.splice(index, 1);
+    res.status(204).send("Deleted!");
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is Listening to Port: ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
