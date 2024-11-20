9ea77471c6bb470f3c6d64650840b95969bb0ea3
ayyush1738 <singhrathoreayush824@gmail.com>
2024-11-21 01:19:24 +0530

    Added videTube folder to main repository

---
+++ videTube/src/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import healthcheckRouter from './routes/healthcheck.routes.js';
+
+const app = express();
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({ extended:true , limit: "16kb"}))
+app.use(express.static("public"))
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+
+export default app;
