dd2cfd9324f70f91d3871f75b2716098354d1e30
ayyush1738 <singhrathoreayush824@gmail.com>
2024-11-27 06:17:03 +0530

    error handling

---
+++ videTube/src/middleware/error.middlewares.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import apiError from "../utils/apiError.js";
+
+const errorHandler = (err, req, res, next) => {
+    let error = err
+}
+
+export default errorHandler;
