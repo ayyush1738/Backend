820d5bbc33872dac32673a6331406f484c722ebb
ayyush1738 <singhrathoreayush824@gmail.com>
2024-12-06 21:43:07 +0530

    backend

--- videTube/src/app.js
+++ videTube/src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import healthcheckRouter from './routes/healthcheck.routes.js';
 import userRouter from './routes/user.routes.js';
+import errorHandler from './middleware/error.middlewares.js';
 
 const app = express();
 app.use(express.json({limit: "16kb"}))
@@ -18,5 +19,7 @@ app.use(
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/user", userRouter)
+
+app.use(errorHandler)
 
 export default app;
--- videTube/src/controllers/user.controllers.js
+++ videTube/src/controllers/user.controllers.js
@@ -3,6 +3,29 @@ import apiError from "../utils/apiError.js";
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
 import User from '../models/user.models.js';
 import apiResponse from "../utils/apiResponse.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
+
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullname, email, username, password } = req.body;
@@ -94,4 +117,103 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) =>{
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+    const {email, username, password} = req.body
+    console.log(email);
+
+    if (!username && !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+    
+    // Here is an alternative of above code based on logic discussed in video:
+    // if (!(username || email)) {
+    //     throw new ApiError(400, "username or email is required")
+        
+    // }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+   const isPasswordValid = await user.isPasswordCorrect(password)
+
+   if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials")
+    }
+
+   const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
+})
+
+const refreshAccessToken = asyncHandler(async (req, ers)=>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if(!incomingRefreshToken)
+    {
+        throw new apiError(401, "Refresh Token is required");
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id);
+        if(!user)
+        {
+            throw new apiError(404, "User not found!/Invallid refresh token!");
+        }
+
+        if(incomingRefreshToken != user?.refreshToken)
+        {
+            throw new apiError(404, "User not found!/Invallid refresh token!");
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        }
+
+        const {refreshToken: newRefreshToken, accessToken} = await generateAccessAndRefereshTokens(user._id);
+
+        return res.status(200).cookei("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(new apiResponse(200, {accessToken, refreshToken: newRefreshToken}, "Access Token Refreshed Successfully"));
+    } catch (error) {
+        throw new apiError(500, "Something went wrong while refreshing the access token");
+    }
+})
+
+export { registerUser, 
+    loginUser,
+    refreshAccessToken
+ };
