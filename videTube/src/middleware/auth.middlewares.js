25d79c074368e9d05cf14088ea052b65d92031c0
ayyush1738 <singhrathoreayush824@gmail.com>
2024-12-11 23:26:51 +0530

    exams :(

---
+++ videTube/src/middleware/auth.middlewares.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import {User} from "../models/user.models.js";
+import {apiResponse} from "../utils/apiResponse.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+    if(!token)
+    {
+        throw new apiError(401, "Unauthorized");
+    }
+
+    try{
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+        if(!user)
+        {
+            throw new apiError(401, "Unauthorized");
+        }
+    }catch(error)
+    {
+        throw new apiError(400, "Unautohrized");
+    }
+
+})
